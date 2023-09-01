db248a5013e4a18d8b7a45056197413b0454d119
Shaun Shanil Menezes <shaunshanilmenezes@Shauns-MacBook-Air.local>
2023-09-01 21:22:15 +0800

    login and signup alerts all good, except login password error i think css issue

--- index.js
+++ index.js
@@ -25,22 +25,29 @@ function validateUser(input) {
 
   if (!("email" in input) || input["email"].length == 0) {
     validationErrors["email"] = "cannot be blank";
-  }
-
-  if (!("password" in input) || input["password"].length == 0) {
-    validationErrors["password"] = "cannot be blank";
-  }
-
-  if ("password" in input && input["password"].length < 8) {
-    validationErrors["password"] = "should be at least 8 characters";
-  }
-
-  if (
+  } else if (
     "email" in input &&
     !input["email"].match(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/)
   ) {
     validationErrors["email"] = "is invalid";
   }
+
+  if (!("password" in input) || input["password"].length == 0) {
+    validationErrors["password"] = "cannot be blank";
+  } else if ("password" in input && input["password"].length < 8) {
+    validationErrors["password"] = "should be at least 8 characters";
+  }
+
+  // if ("password" in input && input["password"].length < 8) {
+  //   validationErrors["password"] = "should be at least 8 characters";
+  // }
+
+  // if (
+  //   "email" in input &&
+  //   !input["email"].match(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/)
+  // ) {
+  //   validationErrors["email"] = "is invalid";
+  // }
 
   return validationErrors;
 }
