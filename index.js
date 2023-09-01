0e15531633fb143b49f9e24de6b5392093b93ed9
Shaun Shanil Menezes <shaunshanilmenezes@Shauns-MacBook-Air.local>
2023-09-01 17:14:22 +0800

    did login but not for login errors

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ import cors from "cors"; // Import the cors middleware
 import prisma from "./src/utils/prisma.js";
 import { Prisma } from "@prisma/client";
 import bcrypt from "bcryptjs";
+import { signAccessToken } from "./src/utils/jwt.js";
+import { filter } from "./src/utils/common.js";
+
+// DATABASE_URL="postgresql://johndoe:randompassword@localhost:5432/mydb?schema=public"
 
 const app = express();
 const port = process.env.PORT || 8080;
@@ -10,9 +14,9 @@ const port = process.env.PORT || 8080;
 app.use(cors()); // Use the cors middleware to allow cross-origin requests
 app.use(express.json()); // Add this middleware to parse JSON in request bodies
 
-function filter(obj, ...keys) {
-  return keys.reduce((a, c) => ({ ...a, [c]: obj[c] }), {});
-}
+// function filter(obj, ...keys) {
+//   return keys.reduce((a, c) => ({ ...a, [c]: obj[c] }), {});
+// }
 
 function validateUser(input) {
   const validationErrors = {};
@@ -82,13 +86,61 @@ app.post("/users", async (req, res) => {
     });
 });
 
+// sign in or AUTH API
+function validateLogin(input) {
+  const validationErrors = {};
+
+  if (!("email" in input) || input["email"].length == 0) {
+    validationErrors["email"] = "cannot be blank";
+  }
+
+  if (!("password" in input) || input["password"].length == 0) {
+    validationErrors["password"] = "cannot be blank";
+  }
+
+  if (
+    "email" in input &&
+    !input["email"].match(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/)
+  ) {
+    validationErrors["email"] = "is invalid";
+  }
+
+  return validationErrors;
+}
+
 app.post("/sign-in", async (req, res) => {
   const data = req.body;
+
+  const validationErrors = validateLogin(data);
+
+  if (Object.keys(validationErrors).length != 0)
+    return res.status(400).send({
+      error: validationErrors,
+    });
+
+  const user = await prisma.user.findUnique({
+    where: {
+      email: data.email,
+    },
+  });
+
+  if (!user)
+    return res.status(401).send({
+      error: "Email address or password not valid",
+    });
+
+  const checkPassword = bcrypt.compareSync(data.password, user.password);
+  if (!checkPassword)
+    return res.status(401).send({
+      error: "Email address or password not valid",
+    });
+
+  const userFiltered = filter(user, "id", "name", "email");
+  const accessToken = await signAccessToken(userFiltered);
+  const userId = user.id;
+  return res.json({ accessToken, userId });
 });
 
 app.listen(port, () => {
   console.log(`App started; listening on port ${port}`);
 });
-
-// ISSUES
-// cant display same email error on browser..
