108386e1c636f6841ec8fbb2d033c26377fd1852
Shaun Shanil Menezes <shaunshanilmenezes@Shauns-MacBook-Air.local>
2023-09-05 11:29:22 +0800

    did migration but dont know if correct

--- app.js
+++ app.js
@@ -3,8 +3,7 @@ import cors from "cors"; // Import the cors middleware
 import userRouter from "./src/controllers/users.controllers.js";
 import authRouter from "./src/controllers/auth.controllers.js";
 import morgan from "morgan";
-
-// import sgMail from "@sendgrid/mail";
+import auth from "./src/middlewares/auth.js"; // tesing DELETE
 
 const app = express();
 app.use(morgan("combined"));
@@ -13,26 +12,10 @@ app.use(express.json()); // Add this middleware to parse JSON in request bodies
 app.use("/users", userRouter);
 app.use("/auth", authRouter);
 
-// // send grid code
-// // const sgMail = require("@sendgrid/mail");
-// sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-
-// const msg = {
-//   to: "goanbeachboy@gmail.com", // Change to your recipient
-//   from: "shaunshanil95@gmail.com", // Change to your verified sender
-//   subject: "Sending with SendGrid is Fun",
-//   text: "and easy to do anywhere, even with Node.js",
-//   html: "<strong>and easy to do anywhere, even with Node.js</strong>",
-// };
-
-// sgMail
-//   .send(msg)
-//   .then((response) => {
-//     console.log(response[0].statusCode);
-//     console.log(response[0].headers);
-//   })
-//   .catch((error) => {
-//     console.error(error);
-//   });
+// DELETE testhing middleware
+app.get("/protected", auth, (req, res) => {
+  res.json({ hello: "world" });
+});
+// DELETE testing finish
 
 export default app;
