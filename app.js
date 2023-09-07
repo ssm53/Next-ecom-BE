d724674be1857ddb99d551808b184093344f575f
Shaun Shanil Menezes <shaunshanilmenezes@Shauns-MacBook-Air.local>
2023-09-07 23:22:33 +0800

    implemented myimages

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ import authRouter from "./src/controllers/auth.controllers.js";
 import uploadRouter from "./src/controllers/upload.controllers.js";
 import allPicsRouter from "./src/controllers/allPics.controllers.js";
 import stripeRouter from "./src/controllers/stripe.controllers.js";
+// import deletePicRouter from "./src/controllers/deletePic.controllers.js";
+import myImagesRouter from "./src/controllers/myImages.controller.js";
 import morgan from "morgan";
 import auth from "./src/middlewares/auth.js"; // tesing DELETE
 
@@ -18,5 +20,7 @@ app.use("/users", userRouter);
 app.use("/auth", authRouter);
 app.use("/upload", uploadRouter);
 app.use("/allPics", allPicsRouter);
+// app.use("/deletePic", deletePicRouter);
+app.use("/my-images/:userId", myImagesRouter);
 
 export default app;
--- src/controllers/allPics.controllers.js
+++ src/controllers/allPics.controllers.js
@@ -7,44 +7,9 @@ import { filter } from "../utils/common.js";
 const router = express.Router();
 
 router.get("/", async (req, res) => {
-  // const data = req.body;
-
-  // const validationErrors = validateUpload(data);
-
-  // if (Object.keys(validationErrors).length != 0)
-  //   return res.status(400).send({
-  //     error: validationErrors,
-  //   });
   const allImages = await prisma.image.findMany();
 
   return res.json({ allImages });
-
-  // data.password = bcrypt.hashSync(data.password, 8);
-  // prisma.image // i changed this to image
-  //   .create({
-  //     data: {
-  //       ...data,
-  //       ownerID: req.user.payload.id,
-  //     },
-  //   })
-  //   .then((image) => {
-  //     return res.json(image);
-  //   })
-  //   .catch((err) => {
-  //     // i dont think we need this anymore
-  //     // if (
-  //     //   err instanceof Prisma.PrismaClientKnownRequestError &&
-  //     //   err.code === "P2002"
-  //     // ) {
-  //     //   const formattedError = {};
-  //     //   formattedError[`${err.meta.target[0]}`] = "already taken";
-
-  //     //   return res.status(500).send({
-  //     //     error: formattedError,
-  //     //   }); // friendly error handling
-  //     // }
-  //     throw err;
-  //   });
 });
 
 export default router;
