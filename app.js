ed6e11e81b636485d9c7677c35f6b6cb588775a9
Shaun Shanil Menezes <shaunshanilmenezes@Shauns-MacBook-Air.local>
2023-09-08 10:54:44 +0800

    alerts and spinners done. authrefresh and redirect my images not yet

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 import express from "express";
 import cors from "cors"; // Import the cors middleware
+import prisma from "./src/utils/prisma.js";
 import userRouter from "./src/controllers/users.controllers.js";
 import authRouter from "./src/controllers/auth.controllers.js";
 import uploadRouter from "./src/controllers/upload.controllers.js";
 import allPicsRouter from "./src/controllers/allPics.controllers.js";
 import stripeRouter from "./src/controllers/stripe.controllers.js";
-import deletePicRouter from "./src/controllers/deletePic.controllers.js";
+// import deletePicRouter from "./src/controllers/deletePic.controllers.js";
 import myImagesRouter from "./src/controllers/myImages.controller.js";
 import morgan from "morgan";
 import auth from "./src/middlewares/auth.js"; // tesing DELETE
@@ -20,7 +21,54 @@ app.use("/users", userRouter);
 app.use("/auth", authRouter);
 app.use("/upload", uploadRouter);
 app.use("/allPics", allPicsRouter);
-app.use("/deletePic/:imageId", deletePicRouter);
+// app.use("/deletePic/:imageId", deletePicRouter);
 app.use("/my-images/:userId", myImagesRouter);
+
+app.delete("/deletePic/:imageId", async (req, res) => {
+  const imageId = parseInt(req.params.imageId);
+  console.log(imageId);
+  console.log(typeof imageId);
+  // but this below doesnt work to get userid
+  // const userId = req.user.payload.id; // Get the ID of the authenticated user
+  // console.log(userId);
+
+  // Use Prisma to find picture, so we can get userId
+  const picture = await prisma.image.findUnique({
+    where: {
+      id: imageId,
+    },
+  });
+  const userId = picture.ownerID;
+  console.log(userId);
+
+  try {
+    // Find the image by ID and ownerID (to ensure ownership)
+    const image = await prisma.image.findUnique({
+      where: {
+        id: imageId,
+        ownerID: userId,
+      },
+    });
+
+    console.log(image);
+
+    if (!image) {
+      // If the image doesn't exist or doesn't belong to the user, return a 404 error
+      return res.status(404).json({ error: "Image not found" });
+    }
+
+    // Delete the image from the database
+    await prisma.image.delete({
+      where: {
+        id: image.id,
+      },
+    });
+
+    return res.status(204).send(); // Successful deletion (status 204)
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ error: "Internal server error" });
+  }
+});
 
 export default app;
--- src/controllers/deletePic.controllers.js
+++ src/controllers/deletePic.controllers.js
@@ -1,52 +1,52 @@
-import express from "express";
-import prisma from "../utils/prisma.js";
-import auth from "../middlewares/auth.js"; // Import the auth middleware to ensure authentication
+// import express from "express";
+// import prisma from "../utils/prisma.js";
+// import auth from "../middlewares/auth.js"; // Import the auth middleware to ensure authentication
 
-const router = express.Router();
+// const router = express.Router();
 
-// Define the delete route
-router.delete("/", async (req, res) => {
-  const { imageId } = req.params;
-  console.log(imageId);
-  // const userId = req.user.payload.id; // Get the ID of the authenticated user
+// // Define the delete route
+// router.delete("/", async (req, res) => {
+//   // const { imageId } = req.params;
+//   console.log(req.params.imageId);
+//   // const userId = req.user.payload.id; // Get the ID of the authenticated user
 
-  // // Use Prisma to find picture, so we can get userId
-  // const picture = await prisma.image.findUnique({
-  //   where: {
-  //     id: imageId,
-  //   },
-  // });
-  // const userId = picture.ownerID;
-  // console.log(userId);
+//   // // Use Prisma to find picture, so we can get userId
+//   // const picture = await prisma.image.findUnique({
+//   //   where: {
+//   //     id: imageId,
+//   //   },
+//   // });
+//   // const userId = picture.ownerID;
+//   // console.log(userId);
 
-  try {
-    // Find the image by ID and ownerID (to ensure ownership)
-    const image = await prisma.image.findUnique({
-      where: {
-        id: +imageId,
-        ownerID: userId,
-      },
-    });
+//   try {
+//     // Find the image by ID and ownerID (to ensure ownership)
+//     const image = await prisma.image.findUnique({
+//       where: {
+//         id: +imageId,
+//         ownerID: userId,
+//       },
+//     });
 
-    console.log(image);
+//     console.log(image);
 
-    if (!image) {
-      // If the image doesn't exist or doesn't belong to the user, return a 404 error
-      return res.status(404).json({ error: "Image not found" });
-    }
+//     if (!image) {
+//       // If the image doesn't exist or doesn't belong to the user, return a 404 error
+//       return res.status(404).json({ error: "Image not found" });
+//     }
 
-    // Delete the image from the database
-    await prisma.image.delete({
-      where: {
-        id: image.id,
-      },
-    });
+//     // Delete the image from the database
+//     await prisma.image.delete({
+//       where: {
+//         id: image.id,
+//       },
+//     });
 
-    return res.status(204).send(); // Successful deletion (status 204)
-  } catch (error) {
-    console.error(error);
-    return res.status(500).json({ error: "Internal server error" });
-  }
-});
+//     return res.status(204).send(); // Successful deletion (status 204)
+//   } catch (error) {
+//     console.error(error);
+//     return res.status(500).json({ error: "Internal server error" });
+//   }
+// });
 
-export default router;
+// export default router;
