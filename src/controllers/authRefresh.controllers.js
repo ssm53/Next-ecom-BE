ee7f6c38944c2e62a4dd16b9e75a223005b9398a
Shaun Shanil Menezes <shaunshanilmenezes@Shauns-MacBook-Air.local>
2023-09-11 09:40:00 +0800

    ready for first deploy signin error verify access token expiry still no

--- src/controllers/authRefresh.controllers.js
+++ src/controllers/authRefresh.controllers.js
@@ -44,6 +44,7 @@ router.post("/", async (req, res) => {
       .status(403)
       .json({ error: "refreshToken does not match the user's refreshToken" });
   } else if (user.refreshToken == refreshToken) {
+    // we should user jwt.verify here, but it doesnt work la!
     return res.json({ accessToken, refreshToken, userId });
   }
 });
@@ -72,83 +73,5 @@ router.post("/", async (req, res) => {
 //     }
 //   }
 // );
-
-// // original 2
-// router.post("/", async (req, res) => {
-//   const data = req.body;
-//   const refreshToken = data.refreshToken;
-//   const userId = parseInt(data.user);
-
-//   const user = await prisma.user.findUnique({
-//     where: {
-//       id: userId,
-//     },
-//   });
-
-//   if (!user) {
-//     return res
-//       .status(401)
-//       .json({ error: "User with the given userId does not exist" });
-//   } else if (!user.refreshToken) {
-//     return res
-//       .status(401)
-//       .json({ error: "refreshToken field is null for the user" });
-//   } else if (user.refreshToken !== refreshToken) {
-//     return res
-//       .status(403)
-//       .json({ error: "refreshToken does not match the user's refreshToken" });
-//   }
-//   console.log("hello");
-
-//   jwt.verify(refreshToken, refreshTokenSecret, async (err, user) => {
-//     if (err) {
-//       return res.status(403).json({ error: "Token verification failed" });
-//     }
-
-//     console.log("hello2");
-
-//     const userFiltered = filter(user, "id", "name", "email");
-//     const accessToken = await signAccessToken(userFiltered);
-
-//     return res.json({ accessToken, refreshToken, userId });
-//   });
-// });
-
-// // original 1
-// router.post("/", async (req, res) => {
-//   const data = req.body;
-//   const refreshToken = data.refreshToken;
-//   const userId = parseInt(data.user); // Convert userId to an integer if it's in string format
-
-//   // here we want to check if the users refreshtoken is null, so for that in our request, we need to send user in te body in our is loggedin function in the frontend. we also need to send the refreshtoken in our body
-//   // now next is check if the user's refresh token is not the same as our refreshtoken variable here, we return res.sendstatus(403)
-
-//   const user = await prisma.user.findUnique({
-//     where: {
-//       id: userId,
-//     },
-//   });
-
-//   if (!user) {
-//     return res.sendStatus(401); // User with the given userId does not exist
-//   } else if (!user.refreshToken) {
-//     return res.sendStatus(401); // refreshToken field is null for the user
-//   } else if (user.refreshToken !== refreshToken) {
-//     return res.sendStatus(403); // refreshToken does not match the user's refreshToken
-//   }
-
-// jwt.verify(
-//   refreshToken,
-//   process.env.refreshTokenSecret,
-//   async (err, user) => {
-//     if (err) return res.sendStatus(403);
-//       const userFiltered = filter(user, "id", "name", "email");
-
-//       const accessToken = await signAccessToken(userFiltered);
-
-//       return res.json({ accessToken, refreshToken, userId });
-//     }
-//   );
-// });
 
 export default router;
